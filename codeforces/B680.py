d42b6a99b758a3b7f46b7ffbcc3b75edf08ad9b3
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2022-07-29 19:56:54 +0300

    Solved 1 code forces B680

---
+++ codeforces/B680.py
@@ -0,0 +1,15 @@
+_, origin = list(map(int, input().split(' ')))
+cities = list(map(int, input().split(' ')))
+origin -= 1
+i, criminals = 1, cities[origin]
+
+while True:
+    if origin+i < len(cities) and origin >= i:
+        criminals += 2 if cities[origin+i] == cities[origin-i] == 1 else 0
+    elif origin+i < len(cities):
+        criminals += cities[origin+i]
+    elif origin >= i:
+        criminals += cities[origin-i]
+    else: break
+    i+=1
+print(criminals)
